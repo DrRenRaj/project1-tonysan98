87bc9582ad6a84517632396095b81ce0eb7f72ee
Tony Sandoval <tsando0304@gmail.com>
2024-07-01 19:50:20 -0600

    Implemented simple login auth

---
+++ app/src/main/java/edu/msudenver/cs3013/project1_s24/LoginActivity.kt
@@ -0,0 +1,36 @@
+package edu.msudenver.cs3013.project1_s24
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private val username = "admin"
+    private val password = "password"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val usernameInput = findViewById<EditText>(R.id.username)
+        val passwordInput = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.btLogin)
+
+        loginButton.setOnClickListener {
+            val inputUsername = usernameInput.text.toString()
+            val inputPassword = passwordInput.text.toString()
+
+            if (inputUsername == username && inputPassword == password) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
